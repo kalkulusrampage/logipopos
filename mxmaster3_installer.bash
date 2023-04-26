addfe263234e080d8ad8431d7406baf4520e884d
kalkulusrampage <121094221+kalkulusrampage@users.noreply.github.com>
2023-04-26 13:27:07 +0200

    Script for automating the process

---
+++ mxmaster3_installer.bash
@@ -0,0 +1,32 @@
+#! /usr/bin/bash
+
+
+
+# build logid
+sudo apt update; wait
+sudo apt install -y cmake libevdev-dev libudev-dev libconfig++-dev; wait
+mkdir build
+cd build
+cmake ..; wait
+make; wait
+sudo make install; wait
+
+# doing configuration file
+cp -v ../.logid.cfg ~/.logid.cfg
+
+# enable/start daemon
+sudo systemctl enable --now logid
+
+# delete volume change annoying sound
+#mv /usr/share/sounds/Pop/stereo/action/audio-volume-change.oga \
+#     /usr/share/sounds/Pop/stereo/action/audio-volume-change.oga.bak
+#
+#mv /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga \
+#     /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga.bak
+
+
+
+
+
+
+
